7abc8a21042d9710976abe6a44ad14ecc515667e
张觉 <21373122@buaa.edu.cn>
2023-05-14 14:41:33 +0800

    init

---
+++ generator.py
@@ -0,0 +1,176 @@
+import random
+import sys
+
+persons = set()
+groups = set()
+messages = set()
+emojis = set()
+maxPersonId = 10000
+maxGroupId = 500
+maxMessageId = 500
+maxEmojiId = 500
+
+def getRandPersonId(p:float=0.8)->str:
+    '''
+        生成一个随机的personId。有p的可能id是已经出现过的
+    '''
+    if len(persons) != 0 and random.randint(0, 100) < 100 * p:
+        return random.choice(list(persons)).__str__()
+    newPersonId = random.randint(-maxPersonId, maxPersonId)
+    persons.add(newPersonId)
+    return str(newPersonId)
+
+def getRandGroupId(p:float=0.8)->str:
+    '''
+        生成一个随机的groupId。有p的可能id是已经出现过的
+    '''
+    if len(groups) != 0 and random.randint(0, 100) < 100 * p:
+        return random.choice(list(groups)).__str__()
+    newGroupId = random.randint(0, maxGroupId)
+    groups.add(newGroupId)
+    return str(newGroupId)
+
+def getRandMessageId(p:float=0.8)->str:
+    '''
+        生成一个随机的messageId。有p的可能id是已经出现过的
+    '''
+    if len(messages) != 0 and random.randint(0, 100) < 100 * p:
+        return random.choice(list(messages)).__str__()
+    newMessageId = random.randint(0, maxMessageId)
+    messages.add(newMessageId)
+    return str(newMessageId)
+
+def getRandStr():
+    return ''.join([random.choice('kjhiuhkjsad8123askj') for i in range(random.randint(1, 10))])
+
+def getRandEmojiId(p:float=0.8)->str:
+    '''
+        生成一个随机的emojiId。有p的可能id是已经出现过的
+    '''
+    if len(emojis) != 0 and random.randint(0, 100) < 100 * p:
+        return random.choice(list(emojis)).__str__()
+    newEmojiId = random.randint(0, maxEmojiId)
+    emojis.add(newEmojiId)
+    return str(newEmojiId)
+
+def getAp()->str:
+    id = getRandPersonId(0.2)
+    name = getRandStr()
+    age = random.randint(0, 201)
+    persons.add(id)
+    return ' '.join(['ap', id, ''.join(name), age.__str__()])
+
+def getAr()->str:
+    return ' '.join(['ar', getRandPersonId(0.8), getRandPersonId(0.8), str(random.randint(1,101))])
+
+def getQv()->str:
+    return ' '.join(['qv', getRandPersonId(0.8), getRandPersonId(0.8)])
+
+def getQci()->str:
+    return ' '.join(['qci', getRandPersonId(0.8), getRandPersonId(0.8)])
+
+def getQbs():
+    return 'qbs'
+
+def getQts():
+    return 'qts'
+
+def getAg():
+    id = getRandGroupId(0.2)
+    groups.add(id)
+    return ' '.join(['ag', id])
+
+def getAtg():
+    id1 = getRandPersonId(0.8)
+    id2 = getRandGroupId(0.8)
+    return ' '.join(['atg', id1, id2])
+
+def getDfg():
+    id1 = getRandPersonId(0.8)
+    id2 = getRandGroupId(0.8)
+    return ' '.join(['dfg', id1, id2])
+
+def getQgvs():
+    return ' '.join(['qgvs', getRandGroupId()])
+
+def getQgav():
+    return ' '.join(['qgav', getRandGroupId()])
+
+def getMr():
+    return ' '.join(['mr', getRandPersonId(0.8), getRandPersonId(0.8), str(random.randint(-100,101))])
+
+def getQba():
+    return ' '.join(['qba', getRandPersonId(0.8)])
+
+def getQcs():
+    return 'qcs'
+
+def getAm():
+    type = random.randint(0,1)
+    if type == 0:
+        return ' '.join(['am', getRandMessageId(0.2), str(random.randint(-1000, 1001)), str(type), getRandPersonId(1), getRandPersonId(1)])
+    else :
+        return ' '.join(['am', getRandMessageId(0.2), str(random.randint(-1000, 1001)), str(type), getRandPersonId(1), getRandGroupId(1)])
+
+def getSm():
+    return ' '.join(['sm', getRandMessageId()])
+
+def getQsv():
+    return ' '.join(['qsv', getRandPersonId()])
+
+def getQrm():
+    return ' '.join(['qrm', getRandPersonId()])
+
+def getArem():
+    type = random.randint(0,1)
+    if type == 0:
+        return ' '.join(['arem', getRandMessageId(0.2), str(random.randint(0, 1001)), str(type), getRandPersonId(1), getRandPersonId(1)])
+    else :
+        return ' '.join(['arem', getRandMessageId(0.2), str(random.randint(0, 1001)), str(type), getRandPersonId(1), getRandGroupId(1)])
+
+def getAnm():
+    type = random.randint(0,1)
+    if type == 0:
+        return ' '.join(['anm', getRandMessageId(0.2), getRandStr(), str(type), getRandPersonId(1), getRandPersonId(1)])
+    else :
+        return ' '.join(['anm', getRandMessageId(0.2), getRandStr(), str(type), getRandPersonId(1), getRandGroupId(1)])
+
+def getCn():
+    return ' '.join(['cn', getRandPersonId()])
+
+def getAem():
+    type = random.randint(0,1)
+    if type == 0:
+        return ' '.join(['aem', getRandMessageId(0.2), getRandEmojiId(), str(type), getRandPersonId(1), getRandPersonId(1)])
+    else :
+        return ' '.join(['aem', getRandMessageId(0.2), getRandEmojiId(), str(type), getRandPersonId(1), getRandGroupId(1)])
+
+def getSei():
+    return ' '.join(['sei', getRandEmojiId(0.2)])
+
+def getQp():
+    return ' '.join(['qp', getRandEmojiId()])
+
+def getDce():
+    return ' '.join(['dce', str(random.randint(0, 5))])
+
+def getQm():
+    return ' '.join(['qm', getRandPersonId()])
+
+def getQlm():
+    return ' '.join(['qlm', getRandPersonId()])
+
+instructions = [getAp, getAr, getQlm]
+
+
+#[getAp, getAr, getQv, getQci, getQbs, getQts, getAg, getAtg, getDfg, getQgvs, getQgav, getMr, getQba, getQcs, getAm, getSm, getQsv, getQrm,
+ #               getArem, getAnm, getCn, getAem, getSei, getQp, getDce, getQm, getQlm]
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    for i in range(n):
+        inst = ''
+        while inst == '':
+            inst = random.choice(instructions)()
+        print(inst)
+    
---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import threading
+import datetime
+jars = ['hw11.jar', 'wsj.jar', 'gyy.jar']  #存放待测的jar包
+max = 1                             #测试的次数
+instructions_num = 10000             #每个测试点的指令数
+
+def runTest(n:int):
+    
+    dir = 'test' + str(n)
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    for file in os.listdir(dir):
+        os.remove(os.path.join(dir, file))
+    
+    dataPath = os.path.join('./' + dir, 'data')    
+    os.system('python generator.py ' + instructions_num.__str__() + ' >' + dataPath)     #30000代表每个数据集的指令数量
+    for jar in jars:
+        start = datetime.datetime.now()
+        os.system('Reader.exe ' + dataPath + ' | java -jar ' + jar + '>' + os.path.join(dir, jar + '.out'))
+        print(jar + ' in test ' + str(n) + ' finished! time:' + str(datetime.datetime.now() - start))
+
+    outputs = []
+    fileName = []
+    for file in os.listdir(dir):
+        if file.endswith('out'):
+            outputs.append(open(os.path.join(dir, file)).readlines())
+            fileName.append(file)
+    dataLines = open(dataPath).readlines()
+
+    for i in range(0, len(outputs[0])):
+        std = outputs[0][i]
+        for j in range(0, len(outputs)):
+            if outputs[j][i] != std:
+                print('something wrong at ' + str(i + 1) + ' line in test ' + str(n))
+                print('The instruction is ' + dataLines[i].strip())
+                for k in range(0, len(outputs)):
+                    print(fileName[k] + ':\t' + outputs[k][i].strip())
+                print('')
+                break
+    
+    print(dir + ' finished!')
+
+n = 1
+lock = threading.Lock()
+def runThread():
+    global n
+    while True:
+        lock.acquire()
+        if n > max:
+            lock.release()
+            break
+        num = n
+        n += 1
+        lock.release()
+        runTest(num)
+
+for i in range(8):
+    t = threading.Thread(target=runThread)
+    t.start()
